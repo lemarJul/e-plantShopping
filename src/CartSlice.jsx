4533395c4f5307da1616e84161c400271bc7d3c4
Julien Lemarchand <lemar.jul@gmail.com>
2024-10-29 16:27:34 +0100

    feat(CartSlice): define addItem, removeItem, updateQuantity reducers

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,41 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+      // Add item to the cart
+      const alreadyAddedProduct = state.items.find(
+        (item) => item.name === name
+      );
+
+      if (alreadyAddedProduct) {
+        alreadyAddedProduct.quantity += 1;
+      } else {
+        state.items.push({
+          name,
+          image,
+          cost,
+          quantity: 1,
+        });
+      }
     },
     removeItem: (state, action) => {
+      console.log({ action });
+      state.items = state.items.filter(
+        (item) => item.name !== action.payload.name
+      );
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find((item) => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
     },
   },
 });
